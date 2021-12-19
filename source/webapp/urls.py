e9f2df52f1966e1b9008c5c8c270e91b985714d9
mtashtanovski <mtashtanovski@gmail.com>
2021-12-19 22:25:17 +0600

    Added final model of game and all html pages and views.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view, game_view
+from webapp.views import index_view, game_view, history_view
 
 urlpatterns = [
     path('', index_view),
-    path('game/', game_view)
+    path('game/', game_view),
+    path('history/', history_view)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
+from .check import Check
 
+rounds_history = {}
+round = 0
 
 # Create your views here.
 def index_view(request):
     return render(request, 'index.html')
+
+
+def game_view(request):
+    global context
+    if request.method == 'GET':
+        return render(request, 'game.html')
+    else:
+        try:
+            context = ''
+            user_numbers = list(map(int, request.POST.get('user_numbers').split()))
+            check = Check(user_numbers)
+            error = check.validation()
+            if error:
+                context += error
+            else:
+                result = check.get_result()
+                context += result
+
+        except ValueError:
+            context += 'The value should be integers'
+
+        global round
+        round += 1
+        rounds_history[round] = context
+
+        print(rounds_history)
+        return render(request, 'game.html', {'context': context})
+
+
+def history_view(request):
+    return render(request, 'history.html', {'rounds_history': rounds_history})
