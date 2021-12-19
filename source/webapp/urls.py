00f4fffac794024f58d6d4807b27557b6a33bf82
mtashtanovski <mtashtanovski@gmail.com>
2021-12-19 13:21:19 +0600

    added new path to gameplay page

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, game_view
 
 urlpatterns = [
     path('', index_view),
+    path('game/', game_view)
 ]
